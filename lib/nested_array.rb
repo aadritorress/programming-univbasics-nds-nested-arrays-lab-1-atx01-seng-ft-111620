c9786c4de3775b7dadf0ad4d3833b8ecd350e25e
Adri Torres <adriana.amaral@msn.com>
2020-11-03 16:37:46 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,7 +40,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-binding.pry
+
   matrix [row][2] << new_value
  
   # Given any matrix (array of arrays), a row index and a column index, 
